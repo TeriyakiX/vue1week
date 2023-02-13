22db80bf47231f34f8d11d9f4531ec7d2522a898
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2023-02-13 13:52:42 +0300

    5 Практическая

--- js/main.js
+++ js/main.js
@@ -5,19 +5,33 @@ new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true ,
-        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        RemoveCart() {
+            this.cart -= 1
+        }
 
     }
+
 })
