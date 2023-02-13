a52b53ea8e230870844ef945be909cb086c06761
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2023-02-13 13:23:42 +0300

    4 Практическая

--- js/main.js
+++ js/main.js
@@ -6,5 +6,18 @@ new Vue({
         altText: "A pair of socks",
         inStock: true ,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
     }
 })
