041a340f7eecc5b266237ac073ee208a17690d79
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2023-02-13 14:46:31 +0300

    6 Практическая

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
+        onSale: true,
         inStock: true ,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -18,7 +21,8 @@ new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        cart: 0
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
 
     },
     methods: {
